4da45b2f7fb4710ed1df8b70b543dc0722ec0892
鐘ケ江周 <kanegaeamane@net79-dhcp177.sfc.keio.ac.jp>
2019-01-08 10:02:02 +0900

    新

--- male.js
+++ male.js
@@ -6,15 +6,40 @@ function　btnLink() {
 var name = document.getElementById("name");
 var age1 = document.getElementById("age1");
 var hobby = document.getElementById("hobby");
-alert(height.value);
-alert(job.value);
-alert(age.value);
   if (height.value=="low" && job.value=="student" && age.value=="teen") {
-    name.innerHTML = "鳴神葵さん"
-    age1.innerHTML = "17才"
+    name.innerHTML = "鳴神葵　さん"
+    age1.innerHTML = "17歳"
     hobby.innerHTML = "映画鑑賞"
 
-  } else {
+  } else if(height.value=="standard" && job.value=="student" && age.value=="teen"){
+    name.innerHTML = "上田さゆり　さん"
+    age1.innerHTML = "21歳"
+    hobby.innerHTML = "料理"
+  } else if (height.value=="high" && job.value=="student" && age.value=="teen") {
+    name.innerHTML = "佐藤優樹菜　さん"
+    age1.innerHTML = "32歳"
+    hobby.innerHTML = "茶道"
 
-  }
+  } else if (height.value=="low" && job.value=="wife" && age.value=="teen") {
+    name.innerHTML = "　島田アンジェリカ　さん"
+    age1.innerHTML = "19歳"
+    hobby.innerHTML = "Rap"
+
+  }else if (height.value=="low" && job.value=="OL" && age.value=="teen"){
+  name.innerHTML = "田中美尋　さん"
+  age1.innerHTML = "18歳"
+  hobby.innerHTML = "お菓子作り"
+
+}else if (height.value=="low" && job.value=="student" && age.value=="young"){
+name.innerHTML = "伊藤聖　さん"
+age1.innerHTML = "21歳"
+hobby.innerHTML ="お酒笑"
+} else if (height.value=="low" && job.value=="student" && age.value=="soso"){
+  name.innerHTML = "二階堂歩美　さん"
+  age1.innerHTML = "35歳"
+  hobby.innerHTML ="日本舞踊"
+}ls
+
+
+
 }
